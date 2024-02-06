a1794c87523b75fce52257095110ee6b989378a5
KudoExtase810 <alaeddino4892@gmail.com>
2024-02-06 17:52:46 +0100

    changing projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,63 +1,6 @@
-import froggerShot from "../../public/images/frogger-sc.png";
-import ovenglowShot from "../../public/images/ovenglow-sc.png";
-import metamadShot from "../../public/images/metamadness-sc.png";
-import sonelgazShot from "../../public/images/sonelgaz-sc.png";
-import afrihaShot from "../../public/images/afriha-sc.png";
-
 const projects = [
     {
-        name: "Bug Tracker",
-        tools: ["React", "TypeScript", "Express.js", "Mongoose", "Netlify"],
-        link: "https://frogger-tracker.netlify.app",
-        role: "Personal project",
-        description:
-            "A project management app that's intended for use as an internal tool within an organization. Users can create projects, assign members, and submit tickets to efficiently track and resolve bugs. Admins have additional privileges to manage other users within the app.",
-        image: froggerShot,
-    },
-
-    {
-        name: "Meta Madness",
-        tools: ["React", "Next.js", "Tailwind", "Vercel", "FramerMotion"],
-        link: "https://metamad.vercel.app",
-        role: "Personal project",
-        description:
-            "A cool looking and well designed landing page that relies on Framer Motion for attractive animations.",
-        image: metamadShot,
-    },
-    {
-        name: "Sonelgaz App",
-        tools: [
-            "React",
-            "TypeScript",
-            "DaisyUI",
-            "Express.js",
-            "Mongoose",
-            "Netlify",
-        ],
-        link: "https://sonelgaz.netlify.app",
-        role: "Company project",
-        description:
-            'A secure role-based MERN stack app made for the company "Sonelgaz" to help manage different requests and forms and keep track of them.',
-        image: sonelgazShot,
-    },
-    {
-        name: "Afriha Shop",
-        tools: [
-            "React",
-            "TypeScript",
-            "Express.js",
-            "Mongoose",
-            "Bootstrap",
-            "SCSS",
-        ],
-        link: "https://afriha.shop",
-        role: "Work project",
-        description:
-            "E-commerce website for selling different types of subscriptions with all the functionalities that an online store might need. The admins can keep track of everything and change what's needed dynamically.",
-        image: afrihaShot,
-    },
-    {
-        name: "Ovenglow",
+        name: "MS Tech",
         tools: [
             "React",
             "TypeScript",
@@ -69,10 +12,10 @@ const projects = [
             "Mapbox",
         ],
         link: "https://ovenglow.vercel.app/login",
-        role: "Personal project",
+        role: "E-commerce website",
         description:
-            "An intuitive website designed for a baker to showcase and sell their products while keeping track of orders and managing them. This site features ecommerce, image upload using cloudinary, localization using the Mapbox API, and much more.",
-        image: ovenglowShot,
+            "A fully featured modern website built using the latest technologies. This platform exemplifies clean design principles and incorporates advanced functionalities, offering users an intuitive browsing experience. With a comprehensive admin panel, administrators have complete control over various aspects of the website. This project demonstrates proficiency in leveraging cutting-edge tools to create efficient and visually appealing online platforms.",
+        image: "/images/projects/ms-tech.png",
     },
 ];
 export default projects;
